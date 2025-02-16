9baac0f058646683c4d8a023c14caa78b32df7c5
MarcinCichy <marcin-cichy@wp.pl>
2025-02-16 01:31:02 +0100

    Changed logger to use "TimedRotatingFileHandler". It will create a new file at midnight, and keep 7 backups.

--- logger.py
+++ logger.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 from colorlog import ColoredFormatter
 
 def setup_logger():
@@ -19,8 +20,13 @@ def setup_logger():
     logger = logging.getLogger('git_sync')
     logger.setLevel(logging.INFO)
 
-    # Handler dla pliku logów
-    file_handler = logging.FileHandler('sync_repos.log')
+    # Handler dla pliku logów z rotacją (np. codziennie o północy)
+    file_handler = TimedRotatingFileHandler(
+        'sync_repos.log',  # nazwa pliku logów
+        when='midnight',   # obracanie pliku o północy
+        interval=1,        # co 1 dzień
+        backupCount=7      # przechowywanie 7 kopii zapasowych
+    )
     file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s'))
     logger.addHandler(file_handler)
 
